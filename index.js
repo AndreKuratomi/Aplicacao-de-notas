223959cfe5eea0d82eea5282c1865b6f37efa8e2
André Kuratomi <andrekuratomi@gmail.com>
2022-01-12 12:03:57 -0300

    feat: base to operade express made and /users CRUD drafted

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const app = express();
+app.listen(3000);
+
+// Rota USERS:
+
+let users = [];
+
+app.post("/users", (req, res) => {
+  // requisição com body
+  newUser = res.json(
+    {
+      id: "algo",
+      name: "fulano",
+      cpf: "cpf",
+      notes: "notes",
+    },
+    "alguma mensagem de sucesso. um header"
+  );
+  users.push(newUser);
+  return newUser, 201;
+});
+
+app.get("/users", (req, res) => {
+  return users, 200;
+});
+
+app.patch("/users/<cpf>", (req, res) => {
+  return userUpdated, 200;
+});
+
+app.delete("/users/<cpf>", (req, res) => {
+  userDeleted = res.json({
+    message: "User is deleted",
+    users: [],
+  });
+});
